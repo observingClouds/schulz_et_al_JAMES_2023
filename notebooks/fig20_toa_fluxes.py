9b5c9022cfad13c1f99b7436a370e519b93d8cff
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2023-06-18 21:15:53 +0200

    update fig20: high clouds option; labels, ...

--- notebooks/fig20_toa_fluxes.py
+++ notebooks/fig20_toa_fluxes.py
@@ -13,11 +13,13 @@
 #     name: covariability
 # ---
 
+import argparse
 import logging
 
 import bokeh
 import dask
 import holoviews as hv
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import mpld3
 import numpy as np
@@ -31,6 +33,26 @@ from holoviews import opts
 from holoviews.plotting.links import DataLink
 from intake import open_catalog
 from omegaconf import OmegaConf
+
+mpl.rcParams["text.usetex"] = True
+mpl.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]  # for \text command
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--highClouds",
+    action="store_true",
+    help="Flag indicating whether high clouds should be included in output figure",
+)
+parser.add_argument(
+    "--no-highClouds",
+    dest="highClouds",
+    action="store_false",
+    help="Flag indicating whether high clouds should be included in output figure",
+)
+parser.set_defaults(highClouds=True)
+
+args = parser.parse_args()
 
 hv.extension("bokeh")
 
@@ -85,9 +107,8 @@ style_dict = {
     "DOM02": {"marker": ".", "color": "orange", "label": "ICON-312m"},
 }
 
-df = pd.read_json(CRE_output_fmt).loc["2020-01-11":"2020-02-18"]
 
-df
+df = pd.read_json(CRE_output_fmt).loc["2020-01-11":"2020-02-18"]
 
 # ## Get mask for high clouds in OBS
 
@@ -112,9 +133,7 @@ q_DOM02 = (
     .compute()
 )
 
-fn = (
-    f"../data/intermediate/Quantile_{threshold_discard_percentile}_brightnessT_GOES16.nc"
-)
+fn = "../data/intermediate/Quantile_brightnessT_GOES16.nc"
 q_ABI = xr.open_dataset(fn)
 q_ABI = q_ABI.sel(time=slice("2020-01-11", "2020-02-18"))
 
@@ -169,10 +188,34 @@ logging.info(df_net.corr())
 
 logging.info(df_net.mean())
 
+logging.info("SW-CRE mean")
+df_cre_sw = df[["swCRE_daily_CERES", "swCRE_daily_DOM01", "swCRE_daily_DOM02"]]
+logging.info(df_cre_sw.mean())
+
+logging.info("LW-CRE mean")
+df_cre_lw = df[["lwCRE_daily_CERES", "lwCRE_daily_DOM01", "lwCRE_daily_DOM02"]]
+logging.info(df_cre_lw.mean())
+
+logging.info("net-CRE mean")
+df_cre_net = df[["netCRE_daily_CERES", "netCRE_daily_DOM01", "netCRE_daily_DOM02"]]
+logging.info(df_cre_net.mean())
+
 logging.info("Shallow cloud days only statistics")
 logging.info(df_net.loc[DOM02_CERES_common_days_without_highclouds].corr())
 
 logging.info(df_net.loc[DOM02_CERES_common_days_without_highclouds].mean())
+
+logging.info("SW-CRE mean")
+df_cre_sw = df[["swCRE_daily_CERES", "swCRE_daily_DOM01", "swCRE_daily_DOM02"]]
+logging.info(df_cre_sw.loc[DOM02_CERES_common_days_without_highclouds].mean())
+
+logging.info("LW-CRE mean")
+df_cre_lw = df[["lwCRE_daily_CERES", "lwCRE_daily_DOM01", "lwCRE_daily_DOM02"]]
+logging.info(df_cre_lw.loc[DOM02_CERES_common_days_without_highclouds].mean())
+
+logging.info("net-CRE mean")
+df_cre_net = df[["netCRE_daily_CERES", "netCRE_daily_DOM01", "netCRE_daily_DOM02"]]
+logging.info(df_cre_net.loc[DOM02_CERES_common_days_without_highclouds].mean())
 
 # ## Visualize cre
 
@@ -187,10 +230,12 @@ max_pattern = ds_max["max_pattern"].loc[DOM02_CERES_common_days_without_highclou
 mean_pattern_freq = ds_max["mean_freq"].loc[DOM02_CERES_common_days_without_highclouds]
 threshold_freq = threshold_freq_default
 
-fig, axs = plt.subplots(1, 3, figsize=(12, 5))
-for p, (product, product_label) in enumerate(
+fig, axs_ = plt.subplots(2, 2, figsize=(8, 8))
+axs = axs_.flatten()
+for pr, (product, product_label) in enumerate(
     zip(["sw", "lw", "net"], ["shortwave", "longwave", "net"])  # noqa: B905
 ):
+    p = pr + 1
     axs[p].set_title(product_label)
     dom01 = df[kf.format(product, "DOM01")].loc[
         DOM01_CERES_common_days_without_highclouds
@@ -202,29 +247,34 @@ for p, (product, product_label) in enumerate(
     ] - np.mean(
         df[kf.format(product, "CERES")].loc[DOM01_CERES_common_days_without_highclouds]
     )
-    ceres_dom02 = df[kf.format(product, "CERES")].loc[
-        DOM02_CERES_common_days_without_highclouds
-    ] - np.mean(
+    ceres_dom02_mean = np.mean(
         df[kf.format(product, "CERES")].loc[DOM02_CERES_common_days_without_highclouds]
     )
-    dom02 = df[kf.format(product, "DOM02")].loc[
-        DOM02_CERES_common_days_without_highclouds
-    ] - np.mean(
+    ceres_dom02 = (
+        df[kf.format(product, "CERES")].loc[DOM02_CERES_common_days_without_highclouds]
+        - ceres_dom02_mean
+    )
+    dom02_mean = np.mean(
         df[kf.format(product, "DOM02")].loc[DOM02_CERES_common_days_without_highclouds]
     )
+    dom02 = (
+        df[kf.format(product, "DOM02")].loc[DOM02_CERES_common_days_without_highclouds]
+        - dom02_mean
+    )
 
-    dom01_all = df[kf.format(product, "DOM01")] - np.mean(
-        df[kf.format(product, "DOM01")]
-    )
-    ceres_dom01_all = df[kf.format(product, "CERES")] - np.mean(
-        df[kf.format(product, "CERES")]
-    )
-    ceres_dom02_all = df[kf.format(product, "CERES")].loc[
-        common_times_DOM02_CERES
-    ] - np.mean(df[kf.format(product, "CERES")].loc[common_times_DOM02_CERES])
-    dom02_all = df[kf.format(product, "DOM02")].loc[common_times_DOM02_CERES] - np.mean(
-        df[kf.format(product, "DOM02")].loc[common_times_DOM02_CERES]
-    )
+    if args.highClouds:
+        dom01_all = df[kf.format(product, "DOM01")] - np.mean(
+            df[kf.format(product, "DOM01")]
+        )
+        ceres_dom01_all = df[kf.format(product, "CERES")] - np.mean(
+            df[kf.format(product, "CERES")]
+        )
+        ceres_dom02_all = df[kf.format(product, "CERES")].loc[
+            common_times_DOM02_CERES
+        ] - np.mean(df[kf.format(product, "CERES")].loc[common_times_DOM02_CERES])
+        dom02_all = df[kf.format(product, "DOM02")].loc[
+            common_times_DOM02_CERES
+        ] - np.mean(df[kf.format(product, "DOM02")].loc[common_times_DOM02_CERES])
 
     # without high clouds
     colors = []
@@ -246,47 +296,64 @@ for p, (product, product_label) in enumerate(
         [regression_func(f) for f in (np.min(dom02), np.max(dom02))],
         color="black",
     )
+    if args.highClouds:
+        y_pos = -30
+    else:
+        y_pos = -28
+
     axs[p].text(
-        -15,
-        -30,
-        r"CRE$_{\mathrm{OBS}}$"
+        -0,
+        -22,
+        r"$\overline{\mathrm{CRE}_{\mathrm{CERES}}}=$" + f"{ceres_dom02_mean:.2f}",
+    )
+    axs[p].text(
+        -3.4,
+        -25,
+        r"$\overline{\mathrm{CRE}_{\mathrm{ICON}\textsf{-}\mathrm{312m}}}=$"
+        + f"{dom02_mean:.2f}",
+    )
+    axs[p].text(
+        -12,
+        y_pos,
+        r"$\mathrm{CRE}'_{\mathrm{CERES}}$"
         + f"={slope:.2f}"
-        + r"$\cdot \mathrm{CRE}_{\mathrm{SIM}}$"
-        + f"+{intercept:.2f}",
+        + r"$\cdot \mathrm{CRE'}_{\mathrm{ICON}\textsf{-}\mathrm{312m}}$",
     )
 
     # with high clouds
-    colors = []
-    for date in dom02_all.index:
-        if max_freq_all.sel(date=date.to_datetime64()) > threshold_freq_all:
-            color = color_dict[
-                mean_pattern_freq_all.pattern.values[
-                    max_pattern_all.sel(date=date.to_datetime64())
+    if args.highClouds:
+        colors = []
+        for date in dom02_all.index:
+            if max_freq_all.sel(date=date.to_datetime64()) > threshold_freq_all:
+                color = color_dict[
+                    mean_pattern_freq_all.pattern.values[
+                        max_pattern_all.sel(date=date.to_datetime64())
+                    ]
                 ]
-            ]
-            colors.append(color)
-        else:
-            colors.append("grey")
-    scatter = axs[p].scatter(
-        dom02_all, ceres_dom02_all, color=colors, alpha=0.2, linewidth=0
-    )
+                colors.append(color)
+            else:
+                colors.append("grey")
+        scatter = axs[p].scatter(
+            dom02_all, ceres_dom02_all, color=colors, alpha=0.2, linewidth=0
+        )
 
-    slope, intercept, _, _, _ = scipy.stats.linregress(x=dom02_all, y=ceres_dom02_all)
-    axs[p].plot(
-        [np.min(dom02_all), np.max(dom02_all)],
-        [regression_func(f) for f in (np.min(dom02_all), np.max(dom02_all))],
-        color="black",
-        alpha=0.2,
-    )
-    axs[p].text(
-        -15,
-        -33,
-        r"CRE$_{\mathrm{OBS}}$"
-        + f"={slope:.2f}"
-        + r"$\cdot \mathrm{CRE}_{\mathrm{SIM}}$"
-        + f"+{intercept:.2f}",
-        alpha=0.2,
-    )
+        slope, intercept, _, _, _ = scipy.stats.linregress(
+            x=dom02_all, y=ceres_dom02_all
+        )
+        axs[p].plot(
+            [np.min(dom02_all), np.max(dom02_all)],
+            [regression_func(f) for f in (np.min(dom02_all), np.max(dom02_all))],
+            color="black",
+            alpha=0.2,
+        )
+        axs[p].text(
+            -15,
+            -33,
+            r"CRE$'_{\mathrm{CERES}}$"
+            + f"={slope:.2f}"
+            + r"$\cdot \mathrm{CRE'}_{\mathrm{CERES}}$",
+            alpha=0.2,
+        )
 
     axs[p].set_aspect(1)
 
@@ -296,10 +363,15 @@ for p, (product, product_label) in enumerate(
         cre_min = min_cre - 1
     if max_cre > cre_max:
         cre_max = max_cre + 1
-    if p == 0:
-        axs[p].set_ylabel("daily CRE anomaly of observations / Wm-2")
-    if p == 1:
-        axs[p].set_xlabel("daily CRE anomaly of simulations / Wm-2")
+    cre_min = -30
+    axs[p].set_ylabel(
+        r"$\mathrm{CRE}_{\mathrm{CERES}}-\overline{\mathrm{CRE}_{\mathrm{CERES}}}$ /"
+        r" Wm$^{-2}$"
+    )
+    axs[p].set_xlabel(
+        r"$\mathrm{CRE}_{\mathrm{ICON-312m}}-\overline{\mathrm{CRE}_{\mathrm{ICON-312m}}}$"
+        r" / Wm$^{-2}$"
+    )
 
     tooltip = mpld3.plugins.PointLabelTooltip(
         scatter, labels=["{0}".format(i.strftime("%d.%m.%Y")) for i in dom02.index]
@@ -324,9 +396,10 @@ plt.savefig("../figures/Daily_CRE_anomaly_obs_vs_sim_dom02.pdf", bbox_inches="ti
 DOM02_CERES_common_days = df_net.index
 
 cre_min = -15
-cre_max = 75
+cre_max = 40
 
-fig, axs = plt.subplots(1, 1)
+fig, axs_ = plt.subplots(2, 2, figsize=(8, 8))
+axs = axs_.flatten()[0]
 
 # without high clouds
 max_freq = ds_max["max_freq"].loc[DOM02_CERES_common_days_without_highclouds]
@@ -354,55 +427,62 @@ axs.plot(
     [regression_func(f) for f in (np.min(sim), np.max(sim))],
     color="black",
 )
+if args.highClouds:
+    y_pos = -8
+else:
+    y_pos = -13
 axs.text(
-    20,
-    -8,
-    r"TOA$_{\mathrm{OBS}}$"
+    5,
+    y_pos,
+    r"TOA$_{\mathrm{CERES}}$"
     + f"={slope:.2f}"
-    + r"$\cdot \mathrm{TOA}_{\mathrm{SIM}}$"
+    + r"$\cdot \mathrm{TOA}_{\mathrm{ICON}\textsf{-}\mathrm{312m}}$"
     + f"+{intercept:.2f}",
 )
 
 # with high clouds
-max_freq = ds_max["max_freq"].loc[DOM02_CERES_common_days]
-max_pattern = ds_max["max_pattern"].loc[DOM02_CERES_common_days]
-mean_pattern_freq = ds_max["mean_freq"].loc[DOM02_CERES_common_days]
-threshold_freq = threshold_freq_default
+if args.highClouds:
+    max_freq = ds_max["max_freq"].loc[DOM02_CERES_common_days]
+    max_pattern = ds_max["max_pattern"].loc[DOM02_CERES_common_days]
+    mean_pattern_freq = ds_max["mean_freq"].loc[DOM02_CERES_common_days]
+    threshold_freq = threshold_freq_default
 
-sim = df_net["net_daily_DOM02"].loc[DOM02_CERES_common_days]
-obs = df_net["net_daily_CERES"].loc[DOM02_CERES_common_days]
+    sim = df_net["net_daily_DOM02"].loc[DOM02_CERES_common_days]
+    obs = df_net["net_daily_CERES"].loc[DOM02_CERES_common_days]
 
-colors = []
-for date in sim.index:
-    if max_freq.sel(date=date.to_datetime64()) > threshold_freq:
-        color = color_dict[
-            mean_pattern_freq.pattern.values[max_pattern.sel(date=date.to_datetime64())]
-        ]
-        colors.append(color)
-    else:
-        colors.append("grey")
-scatter = axs.scatter(sim, obs, color=colors, alpha=0.2)
+    colors = []
+    for date in sim.index:
+        if max_freq.sel(date=date.to_datetime64()) > threshold_freq:
+            color = color_dict[
+                mean_pattern_freq.pattern.values[
+                    max_pattern.sel(date=date.to_datetime64())
+                ]
+            ]
+            colors.append(color)
+        else:
+            colors.append("grey")
+    scatter = axs.scatter(sim, obs, color=colors, alpha=0.2)
 
-slope, intercept, _, _, _ = scipy.stats.linregress(x=sim, y=obs)
-axs.plot(
-    [np.min(sim), np.max(sim)],
-    [regression_func(f) for f in (np.min(sim), np.max(sim))],
-    color="black",
-    alpha=0.2,
-)
-axs.text(
-    20,
-    -13,
-    r"TOA$_{\mathrm{OBS}}$"
-    + f"={slope:.2f}"
-    + r"$\cdot \mathrm{TOA}_{\mathrm{SIM}}$"
-    + f"+{intercept:.2f}",
-    alpha=0.2,
-)
+    slope, intercept, _, _, _ = scipy.stats.linregress(x=sim, y=obs)
+    axs.plot(
+        [np.min(sim), np.max(sim)],
+        [regression_func(f) for f in (np.min(sim), np.max(sim))],
+        color="black",
+        alpha=0.2,
+    )
+    axs.text(
+        20,
+        -13,
+        r"TOA$_{\mathrm{OBS}}$"
+        + f"={slope:.2f}"
+        + r"$\cdot \mathrm{TOA}_{\mathrm{SIM}}$"
+        + f"+{intercept:.2f}",
+        alpha=0.2,
+    )
 
 axs.set_aspect(1)
-axs.set_ylabel("daily net TOA radiation of observations / Wm-2")
-axs.set_xlabel("daily net TOA radiation of simulations / Wm-2")
+axs.set_ylabel("net TOA (CERES) / Wm$^{-2}$")
+axs.set_xlabel("net TOA (ICON-312m) / Wm$^{-2}$")
 
 tooltip = mpld3.plugins.PointLabelTooltip(
     scatter, labels=["{0}".format(i.strftime("%d.%m.%Y")) for i in sim.index]
@@ -411,8 +491,8 @@ mpld3.plugins.connect(fig, tooltip)
 
 axs.set_ylim(cre_min, cre_max)
 axs.set_xlim(cre_min, cre_max)
-axs.set_yticks(np.arange(0, 70, 20))
-axs.set_xticks(np.arange(0, 70, 20))
+axs.set_yticks(np.arange(0, 50, 20))
+axs.set_xticks(np.arange(0, 50, 20))
 axs.plot([cre_max, cre_min], [cre_max, cre_min], color="lightgrey")
 
 plt.tight_layout()
