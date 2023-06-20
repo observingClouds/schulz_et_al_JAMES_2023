ea24289001545e755d28f971eef39f0dff6026f2
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2023-06-20 17:42:44 +0200

    change high cloud calculation

--- src/analysis/create_highcloud_mask.py
+++ src/analysis/create_highcloud_mask.py
@@ -1,96 +1,51 @@
-import glob
-import os
-
 import numpy as np
 import pandas as pd
 import xarray as xr
 from intake import open_catalog
 from omegaconf import OmegaConf
 
-overwrite = False
-fn = "../../data/intermediate/Quantile_brightnessT_GOES16.nc"
-
-cfg = OmegaConf.load("../../config/paths.cfg")
-params = OmegaConf.load("../../config/mesoscale_params.yaml")
+cfg = OmegaConf.load("./config/paths.cfg")
+params = OmegaConf.load("./config/mesoscale_params.yaml")
 cat_tmp = open_catalog(
     "https://raw.githubusercontent.com/eurec4a/eurec4a-intake/master/catalog.yml"
 )
 
-geobounds = {}
-geobounds["lat_min"] = params.metrics.geobounds.lat_min
-geobounds["lat_max"] = params.metrics.geobounds.lat_max
-geobounds["lon_min"] = params.metrics.geobounds.lon_min
-geobounds["lon_max"] = params.metrics.geobounds.lon_max
+valid_cells_limit = 0.9
 
-threshold_discard_percentile = params.metrics.BTbounds.threshold_discard_percentile
-threshold_discard_temperature = params.metrics.BTbounds.threshold_discard_temperature
-
-ds_bt_dom01 = cat_tmp.simulations.ICON.LES_CampaignDomain_control.rttov_DOM01.to_dask()
-ds_bt_dom02 = cat_tmp.simulations.ICON.LES_CampaignDomain_control.rttov_DOM02.to_dask()
-ds_goes16 = xr.open_mfdataset(
-    sorted(
-        glob.glob(
-            cfg.OBS.SATELLITES.GOES16.CH13.filename_fmt_glob.format(date="*", time="*")
-        )
-    ),
-    parallel=True,
+df_goes16_dom1 = xr.open_dataset(
+    cfg.ANALYSIS.MESOSCALE.METRICS.output_filename_fmt.format(
+        type="goes16", DOM=1, exp="_"
+    )[:-4]
+    + ".nc"
 )
-
-print("Data read; starting selection")
-
-BT_DOM01 = ds_bt_dom01.sel(
-    lon=slice(geobounds["lon_min"], geobounds["lon_max"]),
-    lat=slice(geobounds["lat_min"], geobounds["lat_max"]),
-).synsat_rttov_forward_model_1__abi_ir__goes_16__channel_7
-BT_DOM02 = ds_bt_dom02.sel(
-    lon=slice(geobounds["lon_min"], geobounds["lon_max"]),
-    lat=slice(geobounds["lat_min"], geobounds["lat_max"]),
-).synsat_rttov_forward_model_2__abi_ir__goes_16__channel_7
-BT_ABI = ds_goes16.sel(
-    lon=slice(geobounds["lon_min"], geobounds["lon_max"]),
-    lat=slice(geobounds["lat_max"], geobounds["lat_min"]),
-).C13
-
-print("Calculate ICON quantiles")
-
-q_DOM01 = BT_DOM01.quantile(
-    threshold_discard_percentile / 100, dim=["lat", "lon"]
-).compute()
-q_DOM02 = (
-    BT_DOM02.chunk({"lat": 534, "lon": 533, "time": 12})
-    .quantile(threshold_discard_percentile / 100, dim=["lat", "lon"])
-    .compute()
+df_rttov_dom1 = xr.open_dataset(
+    cfg.ANALYSIS.MESOSCALE.METRICS.output_filename_fmt.format(type="rttov", DOM=1, exp=2)
 )
-
-print("Calculate GOES16 quantiles")
-if os.path.exists(fn) and overwrite is False:
-    q_ABI = xr.open_dataset(fn)
-else:
-    sat_input_filename_fmt = cfg.OBS.SATELLITES["GOES16"].CH13.filename_fmt
-    ds_goes16_local = xr.open_mfdataset(
-        sat_input_filename_fmt.replace("%Y%m%d_%H%M", "2020*"), parallel=True
-    )
-    q_ABI = ds_goes16_local.quantile(
-        threshold_discard_percentile / 100, dim=["lat", "lon"]
-    ).compute()
-    q_ABI.to_netcdf(fn)
-q_ABI = q_ABI.sel(time=slice("2020-01-08", "2020-02-18"))
+df_rttov_dom2 = xr.open_dataset(
+    cfg.ANALYSIS.MESOSCALE.METRICS.output_filename_fmt.format(type="rttov", DOM=2, exp=2)
+)
 
 print("High cloud occurrance")
 count_highcloud_occurrance_DOM01 = (
-    (q_DOM01 < threshold_discard_temperature).resample(time="1D").sum()
+    (df_rttov_dom1.valid_cells / df_rttov_dom1.valid_cells.max() < valid_cells_limit)
+    .resample(time="1D")
+    .sum()
 )
 days_without_highclouds_DOM01 = count_highcloud_occurrance_DOM01.where(
     count_highcloud_occurrance_DOM01 == 0, drop=True
 ).time
 count_highcloud_occurrance_DOM02 = (
-    (q_DOM02 < threshold_discard_temperature).resample(time="1D").sum()
+    (df_rttov_dom2.valid_cells / df_rttov_dom2.valid_cells.max() < valid_cells_limit)
+    .resample(time="1D")
+    .sum()
 )
 days_without_highclouds_DOM02 = count_highcloud_occurrance_DOM02.where(
     count_highcloud_occurrance_DOM02 == 0, drop=True
 ).time
 count_highcloud_occurrance_CERES = (
-    (q_ABI.C13 < threshold_discard_temperature).resample(time="1D").sum()
+    (df_goes16_dom1.valid_cells / df_goes16_dom1.valid_cells.max() < valid_cells_limit)
+    .resample(time="1D")
+    .sum()
 )
 days_without_highclouds_CERES = count_highcloud_occurrance_CERES.where(
     count_highcloud_occurrance_CERES == 0, drop=True
@@ -105,7 +60,7 @@ DOM02_CERES_common_days_without_highclouds = list(
 
 pd.DataFrame(
     DOM02_CERES_common_days_without_highclouds, columns=["no_high_cloud"]
-).to_parquet("../../data/result/no_high_clouds_DOM02.pq")
+).to_parquet("./data/result/no_high_clouds_DOM02.pq")
 
 DOM01_CERES_common_days_without_highclouds = list(
     set(days_without_highclouds_CERES.values).intersection(
@@ -114,4 +69,4 @@ DOM01_CERES_common_days_without_highclouds = list(
 )
 pd.DataFrame(
     DOM01_CERES_common_days_without_highclouds, columns=["no_high_cloud"]
-).to_parquet("../../data/result/no_high_clouds_DOM01.pq")
+).to_parquet("./data/result/no_high_clouds_DOM01.pq")
